001c6c8b4df8716be942bb1de236445d47f69eae
daniladzyubenko14-ai <daniladzyubenko14@gmail.com>
2026-07-04 20:35:33 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from config import BOT_TOKEN
+from handlers.start import router as start_router
+
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
+
+dp = Dispatcher()
+dp.include_router(start_router)
+
+
+async def main():
+    print("🚀 Gold Bot запущен")
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
