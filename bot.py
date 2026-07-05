0b25d7da3dfaeaafadebf166a1f223d1b57b49ea
daniladzyubenko14-ai <daniladzyubenko14@gmail.com>
2026-07-05 14:17:33 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,31 @@
 import asyncio
 
-from database import init_db
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 from config import BOT_TOKEN
+from database import init_db   # ✅ важно
+
 from handlers.start import router as start_router
+
 
 bot = Bot(
     token=BOT_TOKEN,
-    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    default=DefaultBotProperties(
+        parse_mode=ParseMode.HTML
+    )
 )
 
 dp = Dispatcher()
 dp.include_router(start_router)
 
+
 async def main():
-    print("🚀 Bot started")
+    await init_db()   # 🔥 ВОТ ЭТО ОБЯЗАТЕЛЬНО
+    print("🚀 Gold Bot запущен")
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
