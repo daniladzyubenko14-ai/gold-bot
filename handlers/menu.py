d75b72c0288fb1ab17d428057f5a0eb3d2c271a2
daniladzyubenko14-ai <daniladzyubenko14@gmail.com>
2026-07-05 21:07:04 +0500

    Update menu.py

--- handlers/menu.py
+++ handlers/menu.py
@@ -1,1 +1,34 @@
+from aiogram import Router, F
+from aiogram.types import CallbackQuery
 
+router = Router()
+
+
+@router.callback_query(F.data == "profile")
+async def profile(callback: CallbackQuery):
+    await callback.answer("Раздел Профиль скоро будет готов 🚀")
+
+
+@router.callback_query(F.data == "referral")
+async def referral(callback: CallbackQuery):
+    await callback.answer("Раздел Реферал скоро будет готов 🚀")
+
+
+@router.callback_query(F.data == "reviews")
+async def reviews(callback: CallbackQuery):
+    await callback.answer("Раздел Отзывы скоро будет готов 🚀")
+
+
+@router.callback_query(F.data == "support")
+async def support(callback: CallbackQuery):
+    await callback.answer("Раздел Техподдержка скоро будет готов 🚀")
+
+
+@router.callback_query(F.data == "info")
+async def info(callback: CallbackQuery):
+    await callback.answer("Раздел Информация скоро будет готов 🚀")
+
+
+@router.callback_query(F.data == "withdraw")
+async def withdraw(callback: CallbackQuery):
+    await callback.answer("Раздел Вывод скоро будет готов 🚀")
