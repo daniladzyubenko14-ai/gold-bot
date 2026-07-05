e52c7c41b627dc2b76b74ba64f2753c5fed3e991
daniladzyubenko14-ai <daniladzyubenko14@gmail.com>
2026-07-05 14:05:03 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,25 +7,17 @@ from aiogram.enums import ParseMode
 from config import BOT_TOKEN
 from handlers.start import router as start_router
 
-from database import unban_user
-
-unban_user(7894106165)
-
 bot = Bot(
     token=BOT_TOKEN,
-    default=DefaultBotProperties(
-        parse_mode=ParseMode.HTML
-    )
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
 )
 
 dp = Dispatcher()
 dp.include_router(start_router)
 
-
 async def main():
-    print("🚀 Gold Bot запущен")
+    print("🚀 Bot started")
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
