52ff108bda6bd6f87bf52edb0315256f4ec94473
daniladzyubenko14-ai <daniladzyubenko14@gmail.com>
2026-07-06 17:06:32 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,28 +7,34 @@ from aiogram.enums import ParseMode
 from config import BOT_TOKEN
 from database import init_db
 
+# Роутеры
 from handlers.start import router as start_router
-from handlers.menu import router as menu_router
 from handlers.profile import router as profile_router
+from handlers.menu import router as menu_router
 from handlers.admin import router as admin_router
 
 
 bot = Bot(
     token=BOT_TOKEN,
-    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    default=DefaultBotProperties(
+        parse_mode=ParseMode.HTML
+    )
 )
 
 dp = Dispatcher()
 
+
+# Порядок подключения важен
 dp.include_router(start_router)
-dp.include_router(menu_router)
 dp.include_router(profile_router)
+dp.include_router(menu_router)
 dp.include_router(admin_router)
 
 
 async def main():
     await init_db()
-    print("🚀 Bot started")
+
+    print("🚀 Gold Bot запущен")
 
     await bot.delete_webhook(drop_pending_updates=True)
 
